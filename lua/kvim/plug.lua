a3fb5ebeba5624f004a070ec8ae588e74f54b314
kvgamedev <kuberrv2005@gmail.com>
2025-09-15 00:06:49 +0530

    (plug)doc: added details about all the fields

--- lua/kvim/plug.lua
+++ lua/kvim/plug.lua
@@ -7,6 +7,19 @@ KPlug.setup = function()
 end
 
 -- Manage Plugin --------------------------------------------------------------
+---src: format: username/repo
+---version: plugin version to use, can be a commit or vim.version.range
+---name: name of the plugin
+---config: function to execute after plugin installation
+---lazy: lazy load the plugin? default false
+---event: if lazy loading, additionally specify the vim event which triggers the installation, eg. "InsertEnter"
+---@class opts: table
+---@field src? string
+---@field name? string
+---@field version? vim.VersionRange|string
+---@field config? function
+---@field lazy? boolean
+---@field event? string
 KPlug.add = function(opts)
 	if opts.lazy then
 		if opts.event then
