bc74acd04fca72d85e6fac6793c827d41d2dc1ad
kvgamedev <kuberrv2005@gmail.com>
2025-09-14 21:26:01 +0530

    (plug): added again because it did not add for some reason

---
+++ lua/kvim/plug.lua
@@ -0,0 +1,67 @@
+local KPlug = {}
+local H = {}
+
+KPlug.setup = function()
+	_G.KPlug = KPlug
+end
+
+KPlug.add = function(opts)
+	if opts.lazy then
+		if opts.event then
+			H.lazy_load(function()
+				H.add_plugin(opts)
+			end, opts.event)
+		else
+			H.lazy_load(function()
+				H.add_plugin(opts)
+			end)
+		end
+		return
+	end
+	H.add_plugin(opts)
+end
+
+H.add_plugin = function(opts)
+	local src = "https://github.com/" .. opts.src
+	if type(opts) == "string" then
+		vim.pack.add({ { src = src } }, { confirm = false })
+		return
+	elseif type(opts) == "table" then
+		if opts.name and opts.version then
+			vim.pack.add({ { src = src, name = opts.name, version = opts.version } }, { confirm = false })
+		elseif opts.name then
+			vim.pack.add({ { src = src, name = opts.name } }, { confirm = false })
+		elseif opts.version then
+			vim.pack.add({ { src = src, version = opts.version } }, { confirm = false })
+		else
+			vim.pack.add({ { src = src } }, { confirm = false })
+		end
+
+		if opts.config then
+			opts.config()
+		end
+	end
+end
+
+local gr = vim.api.nvim_create_augroup("LazyLoad", { clear = true })
+H.lazy_load = function(callback, event)
+	if event then
+		vim.api.nvim_create_autocmd(event, {
+			pattern = "*",
+			once = true,
+			group = gr,
+			callback = callback,
+		})
+		return
+	end
+	vim.api.nvim_create_autocmd("UIEnter", {
+		pattern = "*",
+		once = true,
+		group = gr,
+		callback = function()
+			vim.defer_fn(callback, 0)
+		end,
+	})
+end
+
+return KPlug
