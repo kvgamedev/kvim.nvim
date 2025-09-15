73094c729d4c685fd0d683d0058bd846a9116358
kvgamedev <kuberrv2005@gmail.com>
2025-09-15 13:28:35 +0530

    (plug)refactor: for lazy loading dependencies correctly and stuff

---
+++ lua/kvim/plugrefactor.lua
@@ -0,0 +1,96 @@
+-- find spec tables recursively
+-- find if spec is lazy loaded or not
+-- install spec
+
+local M = {}
+local H = {}
+
+M.add = function(opts)
+	if type(opts) == "string" then
+		H.packadd(opts)
+	else
+		H.check_table(opts)
+	end
+end
+
+H.check_table = function(opts)
+	for _, i in ipairs(opts) do
+		if type(i) == "table" then
+			H.check_table(i)
+			H.install_spec(i)
+		elseif type(i) == "string" then
+			H.packadd(i)
+		end
+	end
+end
+
+H.packadd = function(spec)
+	vim.pack.add({ spec }, { confirm = false })
+end
+
+H.install_spec = function(opts)
+	opts = opts or {}
+
+	if opts.dependencies then
+		M.add(opts.dependencies)
+	end
+
+	opts = H.find_src(opts)
+	if not opts.src then
+		return
+	end
+	opts.src = H.set_src(opts.src)
+
+	if opts.lazy then
+		H.lazy_load(H.exec_installation(opts), opts.event)
+	else
+		H.exec_installation(opts)
+	end
+end
+
+local gr = vim.api.nvim_create_augroup("LazyLoad", { clear = true })
+H.lazy_load = function(callback, event)
+	if event then
+		vim.api.nvim_create_autocmd(event, {
+			pattern = "*",
+			once = true,
+			group = gr,
+			callback = callback,
+		})
+		return
+	end
+	vim.api.nvim_create_autocmd("UIEnter", {
+		pattern = "*",
+		once = true,
+		group = gr,
+		callback = function()
+			vim.defer_fn(callback, 0)
+		end,
+	})
+end
+
+H.exec_installation = function(opts)
+	H.packadd({ src = opts.src, name = opts.name, version = opts.version })
+	opts.config()
+end
+
+H.find_src = function(opts)
+	if not opts.src then
+		for _, i in ipairs(opts) do
+			if type(i) == "string" then
+				opts.src = i
+			end
+		end
+	end
+	return opts
+end
+
+H.set_src = function(src)
+	if src:sub(1, 5) == "https" then
+		return src
+	else
+		return "https://github.com/" .. src
+	end
+end
+
+return M
