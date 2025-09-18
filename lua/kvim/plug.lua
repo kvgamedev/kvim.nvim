77524d8665ba5570db3a1d17175d012d8f394bb4
kvgamedev <kuberrv2005@gmail.com>
2025-09-18 20:12:57 +0530

    (plug): add comments

--- lua/kvim/plug.lua
+++ lua/kvim/plug.lua
@@ -2,6 +2,7 @@
 local M = {}
 local H = {}
 
+-- Add Spec(s)
 function M.add(opts)
 	if type(opts) == "string" then
 		H.packadd(H.set_src(opts))
@@ -10,6 +11,7 @@ function M.add(opts)
 	end
 end
 
+-- Recursively check for spec tables inside spec tables
 function H.check_table(opts)
 	for _, i in ipairs(opts) do
 		if type(i) == "table" then
@@ -22,30 +24,39 @@ function H.check_table(opts)
 	H.install_spec(opts)
 end
 
+-- Add spec
 function H.packadd(spec)
 	vim.pack.add({ spec }, { confirm = false })
 end
 
+-- Get spec data, then execute installation
 function H.install_spec(opts)
 	opts = opts or {}
 
 	opts = H.find_src(opts)
 	if not opts.src then
-		if opts.config then opts.config() end
+		if opts.config then
+			opts.config()
+		end
 	end
 	opts.src = H.set_src(opts.src)
 
 	if opts.lazy then
 		if opts.event then
-			H.lazy_load(function() H.exec_installation(opts) end, opts.event)
+			H.lazy_load(function()
+				H.exec_installation(opts)
+			end, opts.event)
 		else
-			H.lazy_load(function() H.exec_installation(opts) end)
+			H.lazy_load(function()
+				H.exec_installation(opts)
+			end)
 		end
 	else
 		H.exec_installation(opts)
 	end
 end
 
+-- Lazy Load spec
 local gr = vim.api.nvim_create_augroup("LazyLoad", { clear = true })
 function H.lazy_load(callback, event)
 	if event then
@@ -67,15 +78,19 @@ function H.lazy_load(callback, event)
 	})
 end
 
+-- Install spec and execute config function
 function H.exec_installation(opts)
 	if opts.dependencies then
 		M.add(opts.dependencies)
 	end
 
 	H.packadd({ src = opts.src, name = opts.name, version = opts.version })
-	if opts.config then opts.config() end
+	if opts.config then
+		opts.config()
+	end
 end
 
+-- Get source in spec, either a plain string or, spec.src
 function H.find_src(opts)
 	if not opts.src then
 		for _, i in ipairs(opts) do
@@ -87,6 +102,8 @@ function H.find_src(opts)
 	return opts
 end
 
+-- prepend github url, so the user only needs to add user/repository as source
+-- if the src begins with https, then it will be used as is
 function H.set_src(src)
 	if src:sub(1, 5) == "https" then
 		return src
@@ -95,5 +112,6 @@ function H.set_src(src)
 	end
 end
 
+-- Set Global variable KPlug
 _G.KPlug = M
 return M
